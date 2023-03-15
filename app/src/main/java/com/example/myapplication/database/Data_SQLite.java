09e224d1e81e935e782ecd104744e9fee2dc3184
buianhquan <buianhquan2003123@gmail.com>
2023-03-15 22:53:45 +0700

    first commit

---
+++ app/src/main/java/com/example/myapplication/database/Data_SQLite.java
@@ -0,0 +1,9 @@
+package com.example.myapplication.database;
+
+public class  Data_SQLite {
+    public static  final String INSERT_THUTHU = "insert into ThuThu(maTT,hoTen,matKhau) values" +
+            "('admin', 'nguyen admin', 'admin'),"  +
+            "('haivd', 'Vũ Đức Hải', '123456')";
+}
+
+
---
+++ app/src/main/java/com/example/myapplication/manhinhcho.java
@@ -0,0 +1,29 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class manhinhcho extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manhinhcho);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                    Intent intent  = new Intent(manhinhcho.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        },5000 );
+    }
+
+}
